2ff3c072b71fc5d16e1efd7285b7c441cca79e42
rafael1delgado <delgadorafael2011@gmail.com>
2024-06-18 18:54:28 -0400

    feat: add route compress-image

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Jobs\CompressImage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $url = "https://images.pexels.com/photos/18069241/pexels-photo-18069241/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-depicts-how-ai-tools-can-democratise-education-and-make-learning-more-efficient-it-was-created-by-martina-stiftinger-a.png";
-
-    // $url = "https://images.pexels.com/photos/4484078/pexels-photo-4484078.jpeg";
-    CompressImage::dispatch($url);
-
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::post('/compress-image', [HomeController::class, 'compressImage'])->name('compress.image');
