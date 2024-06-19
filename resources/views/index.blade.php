9edcc6a78e3f9919dfff67a47b49e2ec7118ebe9
rafael1delgado <delgadorafael2011@gmail.com>
2024-06-18 22:48:37 -0400

    feat: refresh view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,26 +13,30 @@
 
     </head>
 
-
     <body class="font-sans dark:bg-black dark:text-black/50">
         <!-- component -->
         <div class="h-screen w-screen bg-sky-800">
             <div class="container h-screen mx-auto flex justify-center items-center p-2 md:p-0">
                 <div class="border border-black-300 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
+                    <p class="capitalize md:uppercase text-slate-700 font-bold">
+                        Laravel Compress Image
+                    </p>
 
                     @foreach (['red', 'green', 'sky', 'yellow'] as $key)
                         @if(session()->has($key))
-                            <div class="span bg-{{ $key }}-300 p-2 rounded-md text-gray-800   alert-dismissable">
+                            <div class="span bg-{{ $key }}-300 p-2 rounded-md text-gray-800">
                                 {!! session()->get($key) !!}
                             </div>
                         @endif
                     @endforeach
 
-
-
-
-                    <form class="mt-8 grid grid-cols-4 gap-8" action="{{ route('compress.image') }}" method="POST">
+                    <form
+                        class="mt-3 grid grid-cols-4 gap-8"
+                        action="{{ route('compress.image') }}"
+                        method="POST"
+                    >
                         @csrf
+
                         <div class="col-span-12">
                             <label class="block">
                                 <span class="block text-sm font-medium text-slate-700">
@@ -40,21 +44,30 @@
                                 </span>
 
                                 <input
-                                    type="text"
-                                    name="link"
                                     class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
                                     focus:outline-none focus:border-sky-800 focus:ring-1 focus:ring-sky-800
                                     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                                     invalid:border-pink-500 invalid:text-pink-600
-                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500"/>
+                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                                    name="link"
+                                    type="text"
+                                    placeholder="https://www.dominio.com/imagen-alta-calidad.png"
+                                />
+
+                                <span class="mt-3 text-[10px] leading-6 text-gray-600">
+                                    La imagen se almacenará en
+                                    <strong>public/storage/compress.</strong>
+                                </span>
                             </label>
 
-                            <div class="flex justify-center">
+
+
+                            <div class="flex justify-center mt-5">
                                 <button
-                                    class="m-2 p-2 border-sky-800 rounded-md bg-sky-800 text-white"
+                                    class="p-2 border-sky-800 hover:bg-sky-900 rounded-md bg-sky-800 text-white"
                                     type="submit"
                                 >
-                                    Optimizar
+                                    Comprimir
                                 </button>
                             </div>
                         </div>
